449e6dd8145f587229e926219402267ba3bbf870
c2838 <c2838@yahoo.com.tw>
2024-07-14 22:16:31 +0800

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PortfolioPage, AboutMePage, HomePage } from './assets/page';
-const basename = process.env.PUBLIC_URL
+// const basename = process.env.PUBLIC_URL
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <Routes>
-        <Route path="aboutme" element={<AboutMePage />} />
-        <Route path="portfolio" element={<PortfolioPage />} />
+        <Route path="portfolio-index/aboutme" element={<AboutMePage />} />
+        <Route path="portfolio-index/portfolio" element={<PortfolioPage />} />
         <Route path="*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
--- src/assets/component/LeftNav/LeftNav.jsx
+++ src/assets/component/LeftNav/LeftNav.jsx
@@ -9,10 +9,13 @@ export default function LeftNav() {
         <h4 className={navStyle.subTitle}>produced by C.H.Kao</h4>
       </div>
       <div className={navStyle.indexGroup}>
-        <Link to="/portfolio" className={navStyle.portfolioLink}>
-        My Portfolio
+        <Link
+          to="/portfolio-index/portfolio"
+          className={navStyle.portfolioLink}
+        >
+          My Portfolio
         </Link>
-        <Link to="/aboutme" className={navStyle.aboutMeLink}>
+        <Link to="/portfolio-index/aboutme" className={navStyle.aboutMeLink}>
           About C.H.Kao
         </Link>
       </div>
