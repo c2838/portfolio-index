c2d101e797c9420a0f76186b02448e67d41c1ee1
c2838 <c2838@yahoo.com.tw>
2024-07-14 18:29:55 +0800

    fixed 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PortfolioPage, AboutMePage, HomePage } from './assets/page';
-const basename = "/portfolio-index"
+const basename = process.env.PUBLIC_URL;
 import './App.css'
 
 function App() {
