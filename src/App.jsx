f1b389ecce362840c5bcdb2b44c4d37d566a479b
c2838 <c2838@yahoo.com.tw>
2024-07-14 17:42:54 +0800

    fixed: react-roueter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PortfolioPage, AboutMePage, HomePage } from './assets/page';
+const basename = process.env.PUBLIC_URL;
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="aboutme" element={<AboutMePage />} />
         <Route path="portfolio" element={<PortfolioPage />} />
